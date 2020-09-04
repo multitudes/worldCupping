3cd2fa59a11b2799838b224f9263b4c721f5462d
laurent <wrmultitudes@gmail.com>
2020-09-04 09:41:13 +0200

    Initial Commit

---
+++ worldCupping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  worldCupping
+//
+//  Created by Laurent B on 04/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ worldCupping/worldCuppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  worldCuppingApp.swift
+//  worldCupping
+//
+//  Created by Laurent B on 04/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct worldCuppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2b0342e3b54290a7284bbbeea1650facb205f514
laurent <wrmultitudes@gmail.com>
2020-09-04 17:25:53 +0200

    five rings running at the same time

---
+++ worldCupping/BackgroundTimer.swift
@@ -0,0 +1,35 @@
+//
+//  BackgroundTimer.swift
+//  worldCupping
+//
+//  Created by Laurent B on 04/09/2020.
+//
+
+import SwiftUI
+
+struct BackgroundTimer: View {
+    var geometrySizeWidth: CGFloat
+
+    @Binding var progress: CGFloat
+
+
+    var body: some View {
+        ZStack {
+            // the light red shadow
+            Circle()
+                .stroke(Color.clear, lineWidth: geometrySizeWidth / 3.5)
+                .opacity(0.3)
+            // the white progress circle
+            Circle()
+                .trim(from: 0.0, to: progress / 120)
+                .stroke(
+                    Color.red.opacity(0.2),
+                    style: StrokeStyle(lineWidth: geometrySizeWidth / 2, lineCap: .butt)
+                )
+                .rotationEffect(.degrees(-90))
+        }
+        
+    }
+}
+
+
--- worldCupping/ContentView.swift
+++ worldCupping/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State var showModal: Bool = false
-    @State var progress: CGFloat = 100.0
+    @State var progress: CGFloat = -5
     @State var isOn: Bool = true
     
 
     var body: some View {
         GeometryReader { geometry in
         
+            if isOn {
+                let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+                Text("")
+                    .onReceive(timer) { _ in
+                        if progress < 120 {
+                            progress += 0.1
+                        } else {
+                            isOn = false
+                            // present pop over
+//                            withAnimation(Animation.easeInOut(duration: 0.3)) {
+//                                showPopup = true
+//                            }
+                           
+                        }
+                    }.frame(minWidth: geometry.size.width, alignment: .center)
+            }
             ZStack{
                 SettingsButton(showModal: $showModal)
                 .position(x: geometry.size.width * 0.92, y: geometry.size.width * 0.03)
@@ -23,49 +39,16 @@ struct ContentView: View {
                 .ignoresSafeArea()
                 .padding(.top, 10)
                 
-                TitleView(width: geometry.size.width, height: geometry.size.height)
+              //  TitleView(width: geometry.size.width, height: geometry.size.height)
             
+                
                 ZStack {
                     ZStack {
-                        // the light red shadow
-                        Circle()
-                            .stroke(Color.clear, lineWidth: geometry.size.width / 3.5)
-                            .opacity(0.3)
-                        // the white progress circle
-                        Circle()
-                            .trim(from: 0.0, to: progress / 120)
-                            .stroke(
-                                Color.red.opacity(0.2),
-                                style: StrokeStyle(lineWidth: geometry.size.width / 2, lineCap: .butt)
-                            )
-                            .rotationEffect(.degrees(-90))
-                        
-                        // one circle package
-                        ZStack {
-                            Circle()
-                                .stroke(Color.red, lineWidth: geometry.size.width / 32)
-                                .frame(width: geometry.size.width / 4  , height: geometry.size.width / 2)
-                                .opacity(0.4)
-                            Circle()
-                                .trim(from: 0.0, to: progress / 120)
-                                .stroke(
-                                    Color.blue,
-                                    style: StrokeStyle(lineWidth: geometry.size.width / 32, lineCap: .round)
-                                )
-                                .frame(width: geometry.size.width / 4  , height: geometry.size.width / 2)
-                                .rotationEffect(.degrees(-90))
-                            Text("1")
-                                .foregroundColor(.white)
-                                .font(.largeTitle)
-                                .padding()
-                                .background(Circle())
-                                .foregroundColor(.blue)
-                                .frame(width: geometry.size.width / 4  , height: geometry.size.width / 4)
-                                
-                        }.offset(y: -(geometry.size.width / 3 ))
-                        
-//                        OneCupOfCoffee(geometrySizeWidth: geometry.size.width, cupNumber: "2", showModal: $showModal, progress: $progress, isOn: $isOn, rotationEffectInDegrees: 72.0)
-//                        
+                       
+                        BackgroundTimer(geometrySizeWidth: geometry.size.width, progress: $progress)
+                       
+                        OneCupOfCoffee(geometrySizeWidth: geometry.size.width, cupNumber: 1, showModal: $showModal, progress: $progress, isOn: $isOn, rotationEffectInDegrees: 0)
+                      
                         OneCupOfCoffee(geometrySizeWidth: geometry.size.width, cupNumber: 2, showModal: $showModal, progress: $progress, isOn: $isOn, rotationEffectInDegrees: 72.0)
 
                         OneCupOfCoffee(geometrySizeWidth: geometry.size.width, cupNumber: 3, showModal: $showModal, progress: $progress, isOn: $isOn, rotationEffectInDegrees: 144)
@@ -74,20 +57,6 @@ struct ContentView: View {
 
                         OneCupOfCoffee(geometrySizeWidth: geometry.size.width, cupNumber: 5, showModal: $showModal, progress: $progress, isOn: $isOn, rotationEffectInDegrees: 288)
 
-
-                        // the rounded start
-        //                Circle()
-        //                    .border(Color.clear, width: 0)
-        //                    .frame(width: lineWidth  , height: lineWidth)
-        //                    .foregroundColor(progress != 0 ? .white : Color.clear  )
-        //                    .offset(y: -(frameSize! / 2 )   )
-        //                Circle()
-        //                    .border(Color.clear, width: 0)
-        //                    .frame(width: lineWidth, height: lineWidth)
-        //                    .offset(y: -frameSize! / 2 )
-        //                    .foregroundColor(progress / meditationTime  >= 0.97 ? Color.white : Color.clear)
-        //                    .rotationEffect(Angle.degrees(360 * Double(progress / 120)))
-        //                    .shadow(color: progress / meditationTime >= 0.97 ? Color.black.opacity(0.2): Color.clear, radius: frameSize! / 250 , x: frameSize! / 250, y: 0)
                         
                     }.frame(idealWidth: geometry.size.width/1.5, idealHeight: geometry.size.width / 1.5, alignment: .center)
                     //.rotationEffect(.degrees(-45))

d45cc57f92ea6430580841af7f900f5de7778b54
laurent <wrmultitudes@gmail.com>
2020-09-27 10:27:15 +0200

    Create UserSettings.swift

---
+++ worldCupping/Model/UserSettings.swift
@@ -0,0 +1,13 @@
+//
+//  UserSettings.swift
+//  worldCupping
+//
+//  Created by Laurent B on 27/09/2020.
+//
+
+import Foundation
+import SwiftUI
+
+class UserSettings: ObservableObject {
+    @Published var vibrate: Bool = false
+}
